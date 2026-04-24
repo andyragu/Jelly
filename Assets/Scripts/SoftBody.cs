a1375a2625338b395f14e01b638297d2f8beae0a
andyragu <andyagudelo42@gmail.com>
2026-04-24 19:29:34 -0400

    Connect joints

--- Assets/Scripts/SoftBody.cs
+++ Assets/Scripts/SoftBody.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SoftBody : MonoBehaviour
@@ -11,23 +12,45 @@ public class SoftBody : MonoBehaviour
         Mesh mesh = GetComponent<MeshFilter>().mesh;
 
         Vector3[] vertices = mesh.vertices;
+        int[] triangles = mesh.triangles;
+
+        Rigidbody[] rigidBodies = new Rigidbody[vertices.Length];
 
         for (int i = 0; i < vertices.Length; i++)
         {
             // instantiate a rigid body at the position of each vertex
             GameObject rb = new GameObject();
             rb.AddComponent<Rigidbody>();
+
             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             sphere.transform.position = transform.TransformPoint(vertices[i]);
             sphere.transform.localScale = Vector3.one * 0.08f;
 
-            ConfigurableJoint joint = rb.AddComponent<ConfigurableJoint>();
+            rigidBodies[i] = rb.GetComponent<Rigidbody>();
         }
+
+        HashSet<string> edges = new HashSet<string>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void addEdge(int a, int b, HashSet<string> edges, Rigidbody[] bodies)
     {
-        
+        int min = Mathf.Min(a, b);
+        int max = Mathf.Max(a, b);
+
+        string edgeKey = min + "-" + max;
+
+        if (!edges.Contains(edgeKey)) 
+            edges.Add(edgeKey);
+
+        Rigidbody rbA = bodies[a];
+        Rigidbody rbB = bodies[b];
+
+        ConfigurableJoint joint = rbA.gameObject.AddComponent<ConfigurableJoint>();
+        joint.connectedBody = rbB;
+
+        joint.xMotion = ConfigurableJointMotion.Limited;
+        joint.yMotion = ConfigurableJointMotion.Limited;
+        joint.zMotion = ConfigurableJointMotion.Limited;
     }
+
 }
